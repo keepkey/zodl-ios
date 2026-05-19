6a927c22783257f08f7375073d289d3f815194f3
Greg Nagy <greg@dhamma.works>
2026-05-19 10:55:19 +0200

    Siralop settings

--- secant/Sources/Dependencies/VotingModels/StaticVotingConfig.swift
+++ secant/Sources/Dependencies/VotingModels/StaticVotingConfig.swift
@@ -9,8 +9,8 @@ struct StaticVotingConfig: Codable, Equatable, Sendable {
     static let supportedVersion = 1
     static let algEd25519 = "ed25519"
     static let bundledPinnedSource =
-        "https://raw.githubusercontent.com/valargroup/token-holder-voting-config/d19edb058081d72aba8a955a57607eabf815e5df/static-voting-config.json" +
-        "?checksum=sha256:f56224c6552178e64f9cc75e8ee2a663130c46172b2470fc02efdf94c522f2ac"
+      "https://raw.githubusercontent.com/valargroup/token-holder-voting-config/491e55306aa5c539a0314d30a8b2c51946b88b73/stage/static-voting-config.json" +
+      "?checksum=sha256:80890a6de9acc7293c3e2fabf870bb3e5755dbe0e69de4a59feb8f696134d4dc"
 
     let staticConfigVersion: Int
     let dynamicConfigURL: URL
